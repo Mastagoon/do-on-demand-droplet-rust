3ff1a158d7505a234ca080480db1274ece90f53c
Mastagoon <inzilagiway@gmail.com>
2023-02-28 19:36:27 +0300

    digital ocaen api

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::env::var;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Droplet {
@@ -12,10 +13,7 @@ struct Droplet {
     status: String,
     kernel: Option<Value>,
     created_at: String,
-    features: Vec<String>,
-    backup_ids: Vec<u32>,
     snapshot_ids: Vec<u32>,
-    action_ids: Vec<u32>,
     image: Option<Value>,
     volume_ids: Vec<u32>,
     size: Option<Value>,
@@ -27,11 +25,27 @@ struct Droplet {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct AllDroplets {
+struct DropletCreate {
+    name: String,
+    region: String,
+    size: String,
+    image: String,
+    ssh_keys: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AllDropletsResponse {
     droplets: Vec<Droplet>,
 }
 
-const GET_ALL_DROPLETS: String = "https://api.digitalocean.com/v2/droplets".to_string();
+#[derive(Serialize, Deserialize, Debug)]
+struct CreateDropletResponse {
+    droplet: Droplet,
+}
+
+const GET_ALL_DROPLETS_URL: &str = "https://api.digitalocean.com/v2/droplets";
+const CREATE_DROPLET_URL: &str = "https://api.digitalocean.com/v2/droplets";
+const DELETE_DROPLET_URL: &str = "https://api.digitalocean.com/v2/droplets/";
 
 struct ApiResponse {
     status: u32,
@@ -39,11 +53,18 @@ struct ApiResponse {
     data: Option<Value>,
 }
 
-pub async fn parse_json() {}
-
-pub async fn get(url: String, _params: Option<Vec<String>>) -> ApiResponse {
+async fn get(url: &str, _params: Option<Vec<String>>) -> ApiResponse {
     let client = reqwest::Client::new();
-    let response = match client.get(&url).send().await {
+    println!("GET -> {}", url);
+    let response = match client
+        .get(url)
+        .header(
+            "Authorization",
+            ["Bearer ", &var("DO_TOKEN").expect("DO_TOKEN not set")].join(""),
+        )
+        .send()
+        .await
+    {
         Ok(r) => r,
         Err(e) => {
             return ApiResponse {
@@ -54,8 +75,20 @@ pub async fn get(url: String, _params: Option<Vec<String>>) -> ApiResponse {
         }
     };
 
-    let json: AllDroplets = match response.json().await {
-        Ok(j) => j,
+    println!("{} <-", response.status());
+
+    ApiResponse {
+        status: 200,
+        error: None,
+        data: response.json().await.ok(),
+    }
+}
+
+async fn post(url: &str, body: String) -> ApiResponse {
+    let client = reqwest::Client::new();
+    println!("POST -> {}", url);
+    let response = match client.post(url).body(body).send().await {
+        Ok(r) => r,
         Err(e) => {
             return ApiResponse {
                 status: 500,
@@ -64,74 +97,65 @@ pub async fn get(url: String, _params: Option<Vec<String>>) -> ApiResponse {
             }
         }
     };
-
-    ApiResponse {
+    println!("{} <-", response.status());
+    return ApiResponse {
         status: 200,
         error: None,
-        data: Some(serde_json::to_value(json).unwrap()),
-    }
-
-    // let response = reqwest::get(&url).await;
-    // if response.is_err() {
-    // return ApiResponse {
-    // status: 500,
-    // error: Some("Internal server error.".to_string()),
-    // data: None,
-    // };
-    // };
-    // let result = response.ok().unwrap();
-    // get json from result
-    // let text = result.text().await;
-    // if text.is_err() {
-    // return ApiResponse {
-    // status: 500,
-    // error: Some("Failed to parse".to_string()),
-    // data: None,
-    // };
-    // };
-    // let json: Value = serde_json::from_str(&text.ok().unwrap()).unwrap();
-    // ApiResponse{
-    // status: result.status,
-    // error: None,
-    // }
-    // let result = match reqwest::get(&url).await {
-    // Ok(r) => match r.text().await {
-    // Ok(t) => t,
-    // Err(_) => "error".to_string(),
-    // },
-    // Err(_) => panic!(),
-    // };
-
-    // let body = reqwest::get(url).await.unwrap().text().await.unwrap();
-    // return body;
+        data: response.json().await.ok(),
+    };
 }
 
-pub async fn post(url: String, body: String) -> String {
+async fn delete(url: &str) -> ApiResponse {
     let client = reqwest::Client::new();
-    let res = client.post(url).body(body).send().await.unwrap();
-    let body = res.text().await.unwrap();
-    return body;
+    println!("DELETE -> {}", url);
+    let response = match client.delete(url).send().await {
+        Ok(r) => r,
+        Err(e) => {
+            return ApiResponse {
+                status: 500,
+                error: Some(e.to_string()),
+                data: None,
+            }
+        }
+    };
+    println!("{} <-", response.status());
+    return ApiResponse {
+        status: 200,
+        error: None,
+        data: response.json().await.ok(),
+    };
 }
 
-// export const getAllDroplets = async () => {
-// const result = await get(URLS.GET_ALL_DROPLETS)
-// if (result.status !== 200)
-// Log.error(`getAllDroplets: ${result.error ?? result.status}`)
-// return result.body.droplets as {
-// id: number
-// name: string
-// networks: { v4: { type: string; ip_address: string }[] }
-// status: string
-// }[]
-//
-// }
-
-pub async fn getAllDroplets() {
-    let result = get(GET_ALL_DROPLETS, None).await;
+pub async fn get_all_droplets() -> AllDropletsResponse {
+    let result = get(GET_ALL_DROPLETS_URL, None).await;
     if result.error.is_some() {
         println!("Error: {}", result.error.unwrap());
+        return AllDropletsResponse { droplets: vec![] };
     }
-    let data: AllDroplets = serde_json::from_value(result.data.unwrap()).unwrap();
+    let data: AllDropletsResponse = serde_json::from_value(result.data.unwrap()).unwrap();
     println!("{:?}", data);
-    // const
+    data
+}
+
+pub async fn create_droplet() -> bool {
+    let body = DropletCreate {
+        name: var("DROPLET_NAME").expect("DROPLET_NAME not set"),
+        region: var("DROPLET_REGION").expect("DROPLET_REGION not set"),
+        size: var("DROPLET_SIZE").expect("DROPLET_SIZE not set"),
+        image: var("DO_IMAGE_ID").expect("DROPLET_IMAGE not set"),
+        ssh_keys: var("SSH_FINGERPRINT")
+            .expect("SSH_FINGERPRINT not set")
+            .split(",")
+            .map(|s| s.to_string())
+            .collect(),
+    };
+    let result = post(CREATE_DROPLET_URL, serde_json::to_string(&body).unwrap()).await;
+    if result.error.is_some() {
+        println!("Error: {}", result.error.unwrap());
+        return false;
+    }
+    println!("create droplet result: {:?}", result.data);
+    let data: CreateDropletResponse = serde_json::from_value(result.data.unwrap()).unwrap();
+    println!("{:?}", data);
+    true
 }
