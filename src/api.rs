40f345e759d3e1c03187b4a1a3719f9fc82046d0
Mastagoon <inzilagiway@gmail.com>
2023-02-28 17:14:48 +0300

    restructuring and json is hard man

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,35 @@
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Droplet {
+    id: u32,
+    name: String,
+    memory: u32,
+    vcpus: u32,
+    disk: u32,
+    locked: bool,
+    status: String,
+    kernel: Option<Value>,
+    created_at: String,
+    features: Vec<String>,
+    backup_ids: Vec<u32>,
+    snapshot_ids: Vec<u32>,
+    action_ids: Vec<u32>,
+    image: Option<Value>,
+    volume_ids: Vec<u32>,
+    size: Option<Value>,
+    size_slug: String,
+    networks: Option<Value>,
+    region: Option<Value>,
+    tags: Vec<String>,
+    vpc_uuid: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AllDroplets {
+    droplets: Vec<Droplet>,
+}
 
 const GET_ALL_DROPLETS: String = "https://api.digitalocean.com/v2/droplets".to_string();
 
@@ -11,25 +42,54 @@ struct ApiResponse {
 pub async fn parse_json() {}
 
 pub async fn get(url: String, _params: Option<Vec<String>>) -> ApiResponse {
-    let response = reqwest::get(&url).await;
-    if response.is_err() {
-        return ApiResponse {
-            status: 500,
-            error: Some("Internal server error.".to_string()),
-            data: None,
-        };
+    let client = reqwest::Client::new();
+    let response = match client.get(&url).send().await {
+        Ok(r) => r,
+        Err(e) => {
+            return ApiResponse {
+                status: 500,
+                error: Some(e.to_string()),
+                data: None,
+            }
+        }
     };
-    let result = response.ok().unwrap();
+
+    let json: AllDroplets = match response.json().await {
+        Ok(j) => j,
+        Err(e) => {
+            return ApiResponse {
+                status: 500,
+                error: Some(e.to_string()),
+                data: None,
+            }
+        }
+    };
+
+    ApiResponse {
+        status: 200,
+        error: None,
+        data: Some(serde_json::to_value(json).unwrap()),
+    }
+
+    // let response = reqwest::get(&url).await;
+    // if response.is_err() {
+    // return ApiResponse {
+    // status: 500,
+    // error: Some("Internal server error.".to_string()),
+    // data: None,
+    // };
+    // };
+    // let result = response.ok().unwrap();
     // get json from result
-    let text = result.text().await;
-    if text.is_err() {
-        return ApiResponse {
-            status: 500,
-            error: Some("Failed to parse".to_string()),
-            data: None,
-        };
-    };
-    let json: Value = serde_json::from_str(&text.ok().unwrap()).unwrap();
+    // let text = result.text().await;
+    // if text.is_err() {
+    // return ApiResponse {
+    // status: 500,
+    // error: Some("Failed to parse".to_string()),
+    // data: None,
+    // };
+    // };
+    // let json: Value = serde_json::from_str(&text.ok().unwrap()).unwrap();
     // ApiResponse{
     // status: result.status,
     // error: None,
@@ -42,8 +102,8 @@ pub async fn get(url: String, _params: Option<Vec<String>>) -> ApiResponse {
     // Err(_) => panic!(),
     // };
 
-    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
-    return body;
+    // let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+    // return body;
 }
 
 pub async fn post(url: String, body: String) -> String {
@@ -68,4 +128,10 @@ pub async fn post(url: String, body: String) -> String {
 
 pub async fn getAllDroplets() {
     let result = get(GET_ALL_DROPLETS, None).await;
+    if result.error.is_some() {
+        println!("Error: {}", result.error.unwrap());
+    }
+    let data: AllDroplets = serde_json::from_value(result.data.unwrap()).unwrap();
+    println!("{:?}", data);
+    // const
 }
